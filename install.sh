6131acb3c67cb369d633ab096d33185a78dc2dbb
Todd Wolfson <todd@twolfson.com>
2015-12-24 01:22:49 -0600

    Removed /dev/shm patch since we are on Ubuntu@14.04 (which hopefully is patched)

--- install.sh
+++ install.sh
@@ -26,12 +26,4 @@ elif test "$SUBLIME_TEXT_VERSION" = "3"; then
   sudo add-apt-repository ppa:webupd8team/sublime-text-3 -y
   sudo apt-get update
   sudo apt-get install sublime-text-installer -y
-
-  # If we are in Travis, update shm. Fixes 'Unable to init shm' from `sublime_text --wait`
-  # https://travis-ci.org/twolfson/sublime-plugin-tests/builds/12500309
-  # https://github.com/travis-ci/travis-core/issues/187
-  if test -n "$TRAVIS"; then
-    sudo rmdir /dev/shm
-    sudo ln -Tsf /run/shm /dev/shm
-  fi
 fi
